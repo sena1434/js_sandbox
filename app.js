38d5200c347b54f0e350a51e5b8ef16643ed36fc
Sena's Account <senacetinkaya.tr@gmail.com>
2022-01-17 22:37:07 +0300

    use console

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-alert("Hello World");
+// Log to console
+console.log("Hello World");
+console.log(123);
+console.log(true);
+var greeting = "Hello";
+console.log(greeting);
+console.log([1, 2, 3, 4]); // log with Arrays
+console.log({ a: 1, b: 2 }); // log with Objects
+console.log({ a: 1, b: 2 }); // log with Table
+
+console.error("This is some error");
+console.clear();
+console.warn("This is a warning");
+console.time("Hello");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.timeEnd("Hello");
+/*
+  multi
+  line
+  comments
+*/
