9a61d26e303ca6f64c9aae1790cbd62152674607
Sena's Account <senacetinkaya.tr@gmail.com>
2022-01-19 00:05:39 +0300

    create variables var, let & const

--- app.js
+++ app.js
@@ -1,26 +1,53 @@
-// Log to console
-console.log("Hello World");
-console.log(123);
-console.log(true);
-var greeting = "Hello";
-console.log(greeting);
-console.log([1, 2, 3, 4]); // log with Arrays
-console.log({ a: 1, b: 2 }); // log with Objects
-console.log({ a: 1, b: 2 }); // log with Table
+// // var, let, const
 
-console.error("This is some error");
-console.clear();
-console.warn("This is a warning");
-console.time("Hello");
-console.log("Hello World");
-console.log("Hello World");
-console.log("Hello World");
-console.log("Hello World");
-console.log("Hello World");
-console.log("Hello World");
-console.timeEnd("Hello");
-/*
-  multi
-  line
-  comments
-*/
+// var name = "John Doe";
+// console.log(name);
+// name = "Steve Smith";
+// console.log(name); // You can reassign variables if you use 'var' or 'let'
+
+// // Init var
+// var greeting;
+// console.log(greeting);
+// greeting = "Hello";
+// console.log(greeting);
+
+// // letters, numbers, _, $
+// // Can not start with number
+
+// // Multi word vars
+// var firstName = "John"; // Camel Case
+// var first_name = "Sara"; // Underscore
+// var FirstName = "Tom"; // Pascal Case
+// var firstname;
+
+// LET
+
+// let name; // We can reassign it
+// name = "John Doe";
+// console.log(name);
+// name = "Steve Smith";
+// console.log(name);
+
+//CONST
+// const name = 'John';
+// console.log(name);
+//Can not reassign
+//name = 'Sara';
+// Have to assign a value
+//const greeting;
+
+const person = {
+  name: "John",
+  age: 30,
+};
+
+person.name = "Sara";
+person.age = 32;
+
+// console.log(person);
+
+const nembers = [1, 2, 3, 4, 5];
+numbers.push(6);
+
+console.log(numbers);
+// We can change arrays and objects that are assigned to variables using const
