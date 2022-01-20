49c983e10240493af437715ec02dbbfe7bc0cb43
Sena's Account <senacetinkaya.tr@gmail.com>
2022-01-21 00:02:00 +0300

    type conversion

--- app.js
+++ app.js
@@ -1,27 +1,52 @@
-// PRIMITIVE
+let val;
 
-// String
-const name = "John Doe";
-// Number
-const age = 45;
-// Boolean
-const hasKids = true;
-// Null
-const car = null;
-// Undefined
-let test;
-// Symbol
-const sym = Symbol();
+// Number to string
+val = 5; // 5, number, undefined
+val = String(5); // 5, string, 1
+val = String(4 + 4); // 8, string, 1(because of 8)
 
-// REFERENCE TYPES - Objects
-// Array
-const hobbies = ["movies", "music"];
-// Object literal
-const adress = {
-  city: "Boston",
-  state: "MA",
-};
-const today = new Date();
+// Bool to string
+val = String(true); // true, string, 4
 
-console.log(today);
-console.log(typeof today);
+// Date to string
+val = String(new Date()); // Date, string, 45
+
+// Array to string
+val = String([1, 2, 3, 4]); // 1,2,3,4, string, 7
+
+// toString()
+val = (5).toString(); // 5, string, 1
+val = true.toString(); // true, string, 4
+
+// String to number
+val = "5"; // 5, string, 1
+val = Number("5"); // 5, number, 5
+val = Number(true); // 1, number, 1
+val = Number(false); // 0, number, 0
+val = Number(null); // 0, number, 0
+val = Number("Hello"); // NaN, number, NaN
+val = Number([1, 2, 3]); // NaN, number, NaN
+
+val = parseInt("100.30"); // 100, number, 100
+val = parseFloat("100.30"); // 100.3, number, 100
+
+// Output
+console.log(val);
+console.log(typeof val);
+// console.log(val.length);
+console.log(val.toFixed());
+
+// const val1 = 5;
+// const val2 = 6;
+// const sum = val1 + val2; // 11, number
+
+// const val1 = String(5);
+// const val2 = 6;
+// const sum = val1 + val2; // 56, string
+
+const val1 = String(5);
+const val2 = 6;
+const sum = Number(val1 + val2); // 56, number
+
+console.log(sum);
+console.log(typeof sum);
