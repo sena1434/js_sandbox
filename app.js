47faf446e95e7e554af7e96ee6f186dcf5db6488
Sena's Account <senacetinkaya.tr@gmail.com>
2022-01-21 23:12:37 +0300

    numbers & the math object

--- app.js
+++ app.js
@@ -1,52 +1,27 @@
+const num1 = 100;
+const num2 = 50;
 let val;
 
-// Number to string
-val = 5; // 5, number, undefined
-val = String(5); // 5, string, 1
-val = String(4 + 4); // 8, string, 1(because of 8)
+// Simple math with numbers
+val = num1 + num2;
+val = num1 * num2;
+val = num1 - num2;
+val = num1 / num2;
+val = num1 % num2;
 
-// Bool to string
-val = String(true); // true, string, 4
+// Math Object
+val = Math.PI; // 3.14...
+val = Math.E; // 2.718...
+val = Math.round(2.4); // 2
+val = Math.ceil(2.4); // 3
+val = Math.floor(2.8); // 2
+val = Math.sqrt(64); // 8
+val = Math.abs(-3); // 3
+val = Math.pow(8, 2); // 64
+val = Math.min(2, 33, 4, 1, 55, 6, 3, -2); // -2
+val = Math.max(2, 33, 4, 1, 55, 6, 3, -2); // 55
+val = Math.random(); // random
 
-// Date to string
-val = String(new Date()); // Date, string, 45
+val = Math.floor(Math.random() * 20 + 1); // get a random number between 1 & 20
 
-// Array to string
-val = String([1, 2, 3, 4]); // 1,2,3,4, string, 7
-
-// toString()
-val = (5).toString(); // 5, string, 1
-val = true.toString(); // true, string, 4
-
-// String to number
-val = "5"; // 5, string, 1
-val = Number("5"); // 5, number, 5
-val = Number(true); // 1, number, 1
-val = Number(false); // 0, number, 0
-val = Number(null); // 0, number, 0
-val = Number("Hello"); // NaN, number, NaN
-val = Number([1, 2, 3]); // NaN, number, NaN
-
-val = parseInt("100.30"); // 100, number, 100
-val = parseFloat("100.30"); // 100.3, number, 100
-
-// Output
 console.log(val);
-console.log(typeof val);
-// console.log(val.length);
-console.log(val.toFixed());
-
-// const val1 = 5;
-// const val2 = 6;
-// const sum = val1 + val2; // 11, number
-
-// const val1 = String(5);
-// const val2 = 6;
-// const sum = val1 + val2; // 56, string
-
-const val1 = String(5);
-const val2 = 6;
-const sum = Number(val1 + val2); // 56, number
-
-console.log(sum);
-console.log(typeof sum);
