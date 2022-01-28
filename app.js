ae7dea08a3028814b26d1378033b1fba5653840a
Sena's Account <senacetinkaya.tr@gmail.com>
2022-01-28 12:27:24 +0300

    string methods & concatenation

--- app.js
+++ app.js
@@ -1,27 +1,79 @@
-const num1 = 100;
-const num2 = 50;
+const firstName = "William";
+const lastName = "Johnson";
+const age = 36;
+const str = "Hello there my name is Sena";
+const tags = "web design, web development, programming";
+
 let val;
 
-// Simple math with numbers
-val = num1 + num2;
-val = num1 * num2;
-val = num1 - num2;
-val = num1 / num2;
-val = num1 % num2;
+val = firstName + lastName;
 
-// Math Object
-val = Math.PI; // 3.14...
-val = Math.E; // 2.718...
-val = Math.round(2.4); // 2
-val = Math.ceil(2.4); // 3
-val = Math.floor(2.8); // 2
-val = Math.sqrt(64); // 8
-val = Math.abs(-3); // 3
-val = Math.pow(8, 2); // 64
-val = Math.min(2, 33, 4, 1, 55, 6, 3, -2); // -2
-val = Math.max(2, 33, 4, 1, 55, 6, 3, -2); // 55
-val = Math.random(); // random
+// Concatenation
+val = firstName + " " + lastName;
 
-val = Math.floor(Math.random() * 20 + 1); // get a random number between 1 & 20
+// Append
+val = "Brad ";
+val += "Traversy";
+
+val = "Hello, my name is " + firstName + " and I am " + age;
+
+// ESCAPING \'\'
+val = "That's awesome, I can't wait";
+
+// Length
+val = firstName.length;
+
+// concat
+val = firstName.concat(" ", lastName);
+
+// Change case
+val = firstName.toUpperCase();
+val = firstName.toLowerCase();
+
+val = firstName[2]; // l
+
+// indexOf()
+val = firstName.indexOf("l"); // 2
+val = firstName.lastIndexOf("l"); // 3
+
+// charAt()
+val = firstName.charAt("2"); // l
+
+// Get last char
+val = firstName.charAt(firstName.length - 1); // m
+
+// substring
+val = firstName.substring(0, 4); // Will
+
+// slice()
+val = firstName.slice(0, 4); // Will
+val = firstName.slice(-3); // iam
+
+// split()
+val = str.split(" ");
+/* (6)[("Hello", "there", "my", "name", "is", "Sena")];
+0: "Hello"
+1: "there"
+2: "my"
+3: "name"
+4: "is"
+5: "Sena"
+length: 6
+[[Prototype]]: Array(0) */
+
+val = tags.split(",");
+/*(3)[("web design", " web development", " programming")];
+0: "web design"
+1: " web development"
+2: " programming"
+length: 3
+[[Prototype]]: Array(0) */
+
+// replace()
+val = str.replace("Sena", "Jack"); // "Hello there my name is Jack";
+
+// includes()
+val = str.includes("Hello"); // true
+val = str.includes("Boow!"); // false
 
 console.log(val);
