6f684dea3c74ccdfb31988f3d5b9a8d4f641b097
Sena's Account <senacetinkaya.tr@gmail.com>
2022-02-01 00:41:51 +0300

    general loops

--- app.js
+++ app.js
@@ -1,51 +1,70 @@
-// FUNCTION DECLARATIONS
+// // FOR LOOP
 
-function greet(firstName = "John", lastName = "Doe") {
-  if (typeof firstName === "undefined") {
-    firstName = "John";
-  }
-  if (typeof lastName === "undefined") {
-    lastName = "Doe";
-  }
-  // console.log('Hello');
-  return "Hello " + firstName + " " + lastName;
+// for (let i = 0; i < 10; i++) {
+//   if (i === 2) {
+//     console.log("2 is my favorite number");
+//     continue;
+//   }
+
+//   if (i === 5) {
+//     console.log("Stop the loop");
+//     break;
+//   }
+
+//   console.log("Number " + i);
+// }
+
+// WHILE LOOP
+
+// let i = 0;
+
+// while (i < 10) {
+//   console.log("Number " + i);
+//   i++;
+// }
+
+// DO WHILE LOOP
+// let i = 1000;
+
+// do {
+//   console.log("Number " + i);
+//   i++;
+// } while (i < 10); // always going to run once, no matter what.
+
+// LOOP THROUGH ARRAY
+const cars = ["Ford", "Chevy", "Honda", "Toyata"];
+
+// for (let i = 0; i < cars.length; i++) {
+//   console.log(cars[i]);
+// }
+
+// FOREACH
+// cars.forEach(function (car, index) {
+//   console.log(`${index} : ${car}`);
+//   console.log(array);
+// });
+
+// MAP
+// const users = [
+//   {id:1, name:'John'},
+//   {id:2 name:'Sara'},
+//   {id:3 name:'Karen'},
+//   {id:4 name:'Steve'}
+// ];
+
+// const ids = users.map(function(user){
+//   return user.id;
+// });
+
+// console.log(ids); // (4) [1,2,3,4]
+
+// FOR IN LOOP
+const user = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 40,
+};
+
+for (let x in user) {
+  console.log(`${x} : ${user[x]}`);
 }
-
-// console.log(greet());
-
-// FUNCTION EXPRESIONS
-
-const square = function (x = 3) {
-  return x * x;
-};
-
-// console.log(square());
-
-// IMMIDIATLEY INVOKABLE FUNCTION EXPRESSIONS - IIFEs
-
-// (function(){
-//   console.log('IIFE Ran..');
-// })();
-
-// (function (name) {
-//   console.log("Hello " + name);
-// })("Brad");
-
-// PROPERTY METHODS
-
-const todo = {
-  add: function () {
-    console.log("Add todo..");
-  },
-  edit: function (id) {
-    console.log(`Edit todo ${id}`);
-  },
-};
-
-todo.delete = function () {
-  console.log("Delete todo...");
-};
-
-todo.add();
-todo.edit(22);
-todo.delete();
