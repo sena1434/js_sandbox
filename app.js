bd74cd28fabee11eb5de8eadba05aaf80860bfb5
Sena's Account <senacetinkaya.tr@gmail.com>
2022-02-02 11:25:03 +0300

    window object

--- app.js
+++ app.js
@@ -1,70 +1,57 @@
-// // FOR LOOP
+// WINDOW METHODS / OBJECTS / PROPERTIES
 
-// for (let i = 0; i < 10; i++) {
-//   if (i === 2) {
-//     console.log("2 is my favorite number");
-//     continue;
-//   }
+// Alert
+// window.alert('Hello World');
 
-//   if (i === 5) {
-//     console.log("Stop the loop");
-//     break;
-//   }
+// Prompt
+// const input = prompt();
+// alert(input);
 
-//   console.log("Number " + i);
+// Confirm
+// if (confirm("Are you sure")) {
+//   console.log("YES");
+// } else {
+//   console.log('NO');
 // }
 
-// WHILE LOOP
+let val;
 
-// let i = 0;
+// Outher height and width
+val = window.outerHeight;
+val = window.outerWidth;
 
-// while (i < 10) {
-//   console.log("Number " + i);
-//   i++;
-// }
+// Inner height and width
+val = window.innerHeight;
+val = window.innerWidth;
 
-// DO WHILE LOOP
-// let i = 1000;
+// Scroll points
+val = window.scrollY;
+val = window.scrollX;
 
-// do {
-//   console.log("Number " + i);
-//   i++;
-// } while (i < 10); // always going to run once, no matter what.
+// Location Object
+val = window.location;
+val = window.location.hostname;
+val = window.location.port;
+val = window.location.href;
+val = window.location.search;
 
-// LOOP THROUGH ARRAY
-const cars = ["Ford", "Chevy", "Honda", "Toyata"];
+// Redirect
+// window.location.href = "http://google.com";
+// Reload
+// window.location.reload();
 
-// for (let i = 0; i < cars.length; i++) {
-//   console.log(cars[i]);
-// }
+// History Object
 
-// FOREACH
-// cars.forEach(function (car, index) {
-//   console.log(`${index} : ${car}`);
-//   console.log(array);
-// });
+// window.history.go(-1);
+// val = window.history.length;
 
-// MAP
-// const users = [
-//   {id:1, name:'John'},
-//   {id:2 name:'Sara'},
-//   {id:3 name:'Karen'},
-//   {id:4 name:'Steve'}
-// ];
+// Navigator Object
+val = window.navigator;
+val = window.navigator.appName;
+val = window.navigator.appVersion;
+val = window.navigator.userAgent;
+val = window.navigator.platform;
+val = window.navigator.vendor;
+val = window.navigator.language;
 
-// const ids = users.map(function(user){
-//   return user.id;
-// });
-
-// console.log(ids); // (4) [1,2,3,4]
-
-// FOR IN LOOP
-const user = {
-  firstName: "John",
-  lastName: "Doe",
-  age: 40,
-};
-
-for (let x in user) {
-  console.log(`${x} : ${user[x]}`);
-}
+console.log(val);
