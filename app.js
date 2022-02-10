5d2f1a28f0856367b6cc5d4a5c9b9cd417e46544
Sena's Account <senacetinkaya.tr@gmail.com>
2022-02-10 15:20:53 +0300

    keyboard & input events

--- app.js
+++ app.js
@@ -1,31 +1,40 @@
-const clearBtn = document.querySelector(".clear-tasks");
-const card = document.querySelector(".card");
+const form = document.querySelector("form");
+const taskInput = document.getElementById("task");
 const heading = document.querySelector("h5");
+const select = document.querySelector(`select`);
 
-// Click
-// clearBtn.addEventListener("click", runEvent);
-// Doubleclick
-// clearBtn.addEventListener("dblclick", runEvent);
-// Mousedown
-// clearBtn.addEventListener("mousedown", runEvent);
-// Mousedown
-// clearBtn.addEventListener("mouseup", runEvent);
-// Mouseenter
-// card.addEventListener("mouseenter ", runEvent);
-// Mouseleave
-// card.addEventListener("mouseleave ", runEvent);
-// Mouseover
-// card.addEventListener("mouseover ", runEvent);
-// Mouseout
-// card.addEventListener("mouseout ", runEvent);
-// Mousemove
-card.addEventListener("mousemove", runEvent);
+// Clear Input
+taskInput.value = "";
 
-// Event Handler
+// form.addEventListener("submit", runEvent);
+
+// Keydown
+// taskInput.addEventListener("keydown", runEvent);
+// Keyup
+// taskInput.addEventListener("keyup", runEvent);
+// Keypress
+// taskInput.addEventListener("keypress", runEvent);
+// Focus
+// taskInput.addEventListener("focus", runEvent);
+// Blur
+// taskInput.addEventListener("blur", runEvent);
+//Cut
+// taskInput.addEventListener("cut", runEvent);
+// Paste
+// taskInput.addEventListener("paste", runEvent);
+// input
+// taskInput.addEventListener("input", runEvent);
+// Change
+select.addEventListener("change", runEvent);
+
 function runEvent(e) {
   console.log("EVENT TYPE: ${e.type}");
 
-  heading.textContent = `MouseX: ${e.offsetX} MouseY: ${e.offsetY}`;
+  // console.log(e.target.value);
+  // heading.innerText = e.target.value;
 
-  document.body.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, 40)`;
+  // Get input value
+  // console.log(taskInput.value);
+
+  // e.preventDefault();
 }
